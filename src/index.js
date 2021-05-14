223ba519767f4f6fe9c6b41750840d9b739ba4a9
Paulo Guilherme <paulopgss@gmail.com>
2021-05-14 14:17:15 -0300

    Primeiros requisitos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+});
 
 app.listen(3333);
