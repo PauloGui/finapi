f914eb08c635ceb5366605f7daecbc90c73f26e1
Paulo Guilherme <paulopgss@gmail.com>
2021-05-14 14:57:02 -0300

    Cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
+const { response } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
+app.use(express.json());
+
+const customers = [];
+
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
+
+  const custumerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (custumerAlreadyExists) {
+    return res.status(400).json({ error: "Custumer already exists." });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send("sucess");
 });
 
 app.listen(3333);
